71096fb0295e872c369941bcd3b9be290287bb64
LanzVian <rollgaming6@gmail.com>
2021-09-17 10:22:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const libxzX = require('lib/data/libxzX')
+const libxzY = require('lib/data/libxzY')
+const libxzZ = require('lib/data/libxzZ')
+
+var libxz = function() {
+  
+};
+
+(function() {
+  this.prop = ""
+}).call(libxz.prototype);
+
+exports.libxz = libxz;
